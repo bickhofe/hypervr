6b9391cb35546bcbcc9c1d122eb6e60b3fbc28d8
bickhofe <peter@bickhofe.de>
2019-05-12 22:06:38 +0200

    belt

---
+++ HyperVR/HyperVR/Assets/Scripts/Sticks.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sticks : MonoBehaviour
+{
+    [Range(0f,1f)]
+    public float grip = 0;
+
+    public float startAngle;
+    public float angle;
+
+    public Transform UpperStick;
+    public Transform LowerStick;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpperStick.localEulerAngles = new Vector3(startAngle+grip*angle,0,0);
+    }
+}
