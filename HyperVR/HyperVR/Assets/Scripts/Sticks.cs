3febd3f47a6c721fa1943c008d247a12583e9eda
Peter Bickhofe <peter@bickhofe.de>
2019-05-14 16:44:15 +0200

    garb with stick cklassic

--- HyperVR/HyperVR/Assets/Scripts/Sticks.cs
+++ HyperVR/HyperVR/Assets/Scripts/Sticks.cs
@@ -13,6 +13,10 @@ public class Sticks : MonoBehaviour
     public Transform HandController;
     public Transform UpperStick;
     public Transform LowerStick;
+    public Transform pickup;
+    public Transform curSushi;
+    bool hasItem = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +29,29 @@ public class Sticks : MonoBehaviour
     {
         grip = Input.GetAxis("TriggerRight")*1.25f;
         if (grip > 1) grip = 1;
+        if (hasItem) grip = .25f;
         UpperStick.localEulerAngles = new Vector3(startAngle+grip*angle,0,0);
+
+        if (hasItem && Input.GetAxis("TriggerRight") < .5f) {
+            curSushi.GetComponent<Sushi>().Release();
+            curSushi.transform.parent = null;
+            curSushi = null;
+            hasItem = false;
+        }
     }
+
+    void OnTriggerStay(Collider col){
+        if (col.tag =="GrabObject"){
+            if (!hasItem && grip >.5f){
+                hasItem = true;
+                curSushi = col.transform;
+                curSushi.parent = pickup;
+                curSushi.localPosition = Vector3.zero;
+                curSushi.localEulerAngles = new Vector3(0,curSushi.localEulerAngles.y,0);
+                curSushi.GetComponent<Sushi>().Grab();
+            }
+        }
+    }
+
+
 }
---
+++ HyperVR/HyperVR/Assets/Scripts/Sushi.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sushi : MonoBehaviour
+{
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Grab(){
+        rb.isKinematic = true;
+    }
+
+    public void Release(){
+        rb.isKinematic = false;
+    }
+}
