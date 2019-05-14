14071e63d1edbf1cb7287bff871b50502bff3df9
Peter Bickhofe <peter@bickhofe.de>
2019-05-14 13:38:00 +0200

    chopsticks vr

--- HyperVR/HyperVR/Assets/Scripts/Sticks.cs
+++ HyperVR/HyperVR/Assets/Scripts/Sticks.cs
@@ -10,17 +10,21 @@ public class Sticks : MonoBehaviour
     public float startAngle;
     public float angle;
 
+    public Transform HandController;
     public Transform UpperStick;
     public Transform LowerStick;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.parent = HandController;
+        transform.localPosition = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
+        grip = Input.GetAxis("TriggerRight")*1.25f;
+        if (grip > 1) grip = 1;
         UpperStick.localEulerAngles = new Vector3(startAngle+grip*angle,0,0);
     }
 }
